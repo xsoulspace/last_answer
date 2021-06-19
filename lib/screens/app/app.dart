1b05368cf757237a8a0a1c895d2cb4e62a9ffd49
Anton Malofeev <anton@xsoulspace.dev>
2021-06-19 15:42:04 +0300

    refactor: all files to old

---
+++ lib/screens/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

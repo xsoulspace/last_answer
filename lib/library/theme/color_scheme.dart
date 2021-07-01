05cb6eba96264e625ddad2b344e4f3e9681e2fec
Anton Malofeev <anton@xsoulspace.dev>
2021-07-01 12:20:36 +0300

    wip: colors

--- lib/library/theme/color_scheme.dart
+++ lib/library/theme/color_scheme.dart
@@ -2,9 +2,9 @@ part of 'theme.dart';
 
 final lightColorScheme = ColorScheme(
   primary: AppColors.primary,
-  primaryVariant: primaryVariant,
-  secondary: secondary,
-  secondaryVariant: secondaryVariant,
+  primaryVariant: AppColors.primary1,
+  secondary: AppColors.grey4,
+  secondaryVariant: AppColors.grey3,
   surface: AppColors.cleanWhite,
   background: AppColors.white,
   error: AppColors.accent1,
@@ -18,9 +18,9 @@ final lightColorScheme = ColorScheme(
 
 final darkColorScheme = ColorScheme(
   primary: AppColors.primary,
-  primaryVariant: primaryVariant,
-  secondary: secondary,
-  secondaryVariant: secondaryVariant,
+  primaryVariant: AppColors.primary1,
+  secondary: AppColors.grey2,
+  secondaryVariant: AppColors.grey1,
   surface: AppColors.black,
   background: AppColors.black,
   error: AppColors.accent1,

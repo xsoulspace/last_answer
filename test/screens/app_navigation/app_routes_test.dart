edb6f0891b881df0c8496d5c3ad434970c82e0cf
Anton Malofeev <anton@xsoulspace.dev>
2021-08-04 00:47:58 +0300

    feat: test to verify routes

--- test/screens/app_navigation/app_routes_test.dart
+++ test/screens/app_navigation/app_routes_test.dart
@@ -1,5 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:lastanswer/screens/app_navigator/app_navigator.dart';
 
 void main() {
-  group('[app route config]', () {});
+  group('[app route config]', () {
+    test('has no empty routes', () {
+      for (final routeName in AppRoutesName.values) {
+        expect(routeName, isNotEmpty);
+      }
+    });
+  });
 }

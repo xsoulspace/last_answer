8b47cdabd9dba3bea91fd32c7bca962f52bb6db7
Anton Malofeev <anton@xsoulspace.dev>
2021-11-26 02:10:31 +0300

    refactor: init stores  function

--- lib/screens/app/app_store_initializer.dart
+++ lib/screens/app/app_store_initializer.dart
@@ -17,6 +17,7 @@ class AppStoreInitializer extends ConsumerWidget {
     final Key? key,
   }) : super(key: key);
   final Widget child;
+
   @override
   Widget build(final BuildContext context, final WidgetRef ref) {
     final settings = SettingsStateScope.of(context);
@@ -24,143 +25,7 @@ class AppStoreInitializer extends ConsumerWidget {
       return child;
     }
     return FutureBuilder<bool>(
-      future: () async {
-        if (settings.appInitialStateLoaded) {
-          return !settings.appInitialStateIsLoading;
-        }
-        settings
-          ..appInitialStateLoaded = true
-          ..appInitialStateIsLoading = true;
-
-        final settingsState = SettingsStateScope.of(context);
-        await settingsState.load();
-
-        settingsState.loadingStatus = AppStateLoadingStatuses.emoji;
-        // ignore: use_build_context_synchronously
-        final emojis = await EmojiUtil.getList(context);
-
-        ref
-            .read(emojisProvider.notifier)
-            .putEntries(emojis.map((final e) => MapEntry(e.emoji, e)));
-
-        // ignore: use_build_context_synchronously
-        final specialEmojis = await EmojiUtil.getSpecialList(context);
-        ref
-            .read(specialEmojisProvider.notifier)
-            .putEntries(specialEmojis.map((final e) => MapEntry(e.emoji, e)));
-
-        final lastUsedEmojis = await EmojiUtil().load();
-        ref.read(lastUsedEmojisProvider.notifier).putAll(lastUsedEmojis);
-
-        settings.loadingStatus = AppStateLoadingStatuses.ideas;
-
-        await Hive.openBox<IdeaProjectAnswer>(
-          HiveBoxesIds.ideaProjectAnswerKey,
-        );
-
-        final ideas =
-            await Hive.openBox<IdeaProject>(HiveBoxesIds.ideaProjectKey);
-
-        settings.loadingStatus = AppStateLoadingStatuses.questionsForAnswers;
-
-        final questions = await Hive.openBox<IdeaProjectQuestion>(
-          HiveBoxesIds.ideaProjectQuestionKey,
-        );
-
-        // TODO(arenukvern): comment when all devices will be updated
-        if (questions.isEmpty) {
-          await questions.putAll(
-            Map.fromEntries(
-              _initialQuestions.map((final e) => MapEntry(e.id, e)),
-            ),
-          );
-        }
-
-        settings.loadingStatus = AppStateLoadingStatuses.answersForIdeas;
-
-        MapState.load(
-          ref: ref,
-          provider: ideaProjectQuestionsProvider,
-          box: questions,
-        );
-
-        settings.loadingStatus = AppStateLoadingStatuses.answersForIdeas;
-
-        final ideaProjectsState = MapState.load(
-          ref: ref,
-          provider: ideaProjectsProvider,
-          box: ideas,
-        );
-
-        settings.loadingStatus = AppStateLoadingStatuses.notes;
-
-        final notes = await Hive.openBox<NoteProject>(
-          HiveBoxesIds.noteProjectKey,
-        );
-
-        final notesProjectsState = MapState.load(
-          ref: ref,
-          provider: noteProjectsProvider,
-          box: notes,
-        );
-
-        await Hive.openBox<StoryProject>(
-          HiveBoxesIds.storyProjectKey,
-        );
-
-        final projectsFolders = await Hive.openBox<ProjectFolder>(
-          HiveBoxesIds.projectFolderKey,
-        );
-
-        final projectsService = BasicProjectsService(
-          ideas: ideaProjectsState.safeState,
-          notes: notesProjectsState.safeState,
-          // TODO(arenukvern): add stories in v4
-          stories: const {},
-        );
-
-        ProjectFolder currentFolder;
-
-        /// case if project folders is not created
-        if (projectsFolders.isEmpty) {
-          currentFolder = (await ProjectFolder.create())
-            ..addProjects([
-              ...ideaProjectsState.safeState.values,
-              ...notesProjectsState.safeState.values,
-            ]);
-        } else {
-          MapState.load(
-            ref: ref,
-            provider: projectsFoldersProvider,
-            box: projectsFolders,
-          );
-
-          for (final projectsFolder in projectsFolders.values) {
-            ProjectFolder.loadProjectsFromService(
-              folder: projectsFolder,
-              service: projectsService,
-            );
-          }
-
-          // TODO(arenukvern): add last used folder
-          currentFolder = projectsFolders.values.first;
-        }
-
-        ref.read(currentFolderProvider.notifier).state = currentFolder;
-
-        // TODO(arenukvern): in case of future migrations
-        // settings.loadingStatus = AppStateLoadingStatuses.migratingOldData;
-        // if (!settings.migrated) {
-        //   await settings.setMigrated();
-        // }
-
-        /// ***************** MIGRATION END *******************
-        settings.appInitialStateIsLoading = false;
-        WidgetsBinding.instance?.addPostFrameCallback((final _) {
-          settings.notify();
-        });
-        return true;
-      }(),
+      future: initStores(context: context, ref: ref, settings: settings),
       builder: (final context, final snapshot) {
         if (snapshot.connectionState != ConnectionState.done ||
             snapshot.data == false) {
@@ -190,5 +55,146 @@ class AppStoreInitializer extends ConsumerWidget {
         return child;
       },
     );
+  }
+
+  Future<bool> initStores({
+    required final SettingsController settings,
+    required final WidgetRef ref,
+    required final BuildContext context,
+  }) async {
+    if (settings.appInitialStateLoaded) {
+      return !settings.appInitialStateIsLoading;
+    }
+    settings
+      ..appInitialStateLoaded = true
+      ..appInitialStateIsLoading = true;
+
+    final settingsState = SettingsStateScope.of(context);
+    await settingsState.load();
+
+    settingsState.loadingStatus = AppStateLoadingStatuses.emoji;
+    // ignore: use_build_context_synchronously
+    final emojis = await EmojiUtil.getList(context);
+
+    ref
+        .read(emojisProvider.notifier)
+        .putEntries(emojis.map((final e) => MapEntry(e.emoji, e)));
+
+    // ignore: use_build_context_synchronously
+    final specialEmojis = await EmojiUtil.getSpecialList(context);
+    ref
+        .read(specialEmojisProvider.notifier)
+        .putEntries(specialEmojis.map((final e) => MapEntry(e.emoji, e)));
+
+    final lastUsedEmojis = await EmojiUtil().load();
+    ref.read(lastUsedEmojisProvider.notifier).putAll(lastUsedEmojis);
+
+    settings.loadingStatus = AppStateLoadingStatuses.ideas;
+
+    await Hive.openBox<IdeaProjectAnswer>(
+      HiveBoxesIds.ideaProjectAnswerKey,
+    );
+
+    final ideas = await Hive.openBox<IdeaProject>(HiveBoxesIds.ideaProjectKey);
+
+    settings.loadingStatus = AppStateLoadingStatuses.questionsForAnswers;
+
+    final questions = await Hive.openBox<IdeaProjectQuestion>(
+      HiveBoxesIds.ideaProjectQuestionKey,
+    );
+
+    // TODO(arenukvern): comment when all devices will be updated
+    if (questions.isEmpty) {
+      await questions.putAll(
+        Map.fromEntries(
+          _initialQuestions.map((final e) => MapEntry(e.id, e)),
+        ),
+      );
+    }
+
+    settings.loadingStatus = AppStateLoadingStatuses.answersForIdeas;
+
+    MapState.load(
+      ref: ref,
+      provider: ideaProjectQuestionsProvider,
+      box: questions,
+    );
+
+    settings.loadingStatus = AppStateLoadingStatuses.answersForIdeas;
+
+    final ideaProjectsState = MapState.load(
+      ref: ref,
+      provider: ideaProjectsProvider,
+      box: ideas,
+    );
+
+    settings.loadingStatus = AppStateLoadingStatuses.notes;
+
+    final notes = await Hive.openBox<NoteProject>(
+      HiveBoxesIds.noteProjectKey,
+    );
+
+    final notesProjectsState = MapState.load(
+      ref: ref,
+      provider: noteProjectsProvider,
+      box: notes,
+    );
+
+    await Hive.openBox<StoryProject>(
+      HiveBoxesIds.storyProjectKey,
+    );
+
+    final projectsFolders = await Hive.openBox<ProjectFolder>(
+      HiveBoxesIds.projectFolderKey,
+    );
+
+    final projectsService = BasicProjectsService(
+      ideas: ideaProjectsState.safeState,
+      notes: notesProjectsState.safeState,
+      // TODO(arenukvern): add stories in v4
+      stories: const {},
+    );
+
+    ProjectFolder currentFolder;
+
+    /// case if project folders is not created
+    if (projectsFolders.isEmpty) {
+      currentFolder = (await ProjectFolder.create())
+        ..addProjects([
+          ...ideaProjectsState.safeState.values,
+          ...notesProjectsState.safeState.values,
+        ]);
+    } else {
+      MapState.load(
+        ref: ref,
+        provider: projectsFoldersProvider,
+        box: projectsFolders,
+      );
+
+      for (final projectsFolder in projectsFolders.values) {
+        ProjectFolder.loadProjectsFromService(
+          folder: projectsFolder,
+          service: projectsService,
+        );
+      }
+
+      // TODO(arenukvern): add last used folder
+      currentFolder = projectsFolders.values.first;
+    }
+
+    ref.read(currentFolderProvider.notifier).state = currentFolder;
+
+    // TODO(arenukvern): in case of future migrations
+    // settings.loadingStatus = AppStateLoadingStatuses.migratingOldData;
+    // if (!settings.migrated) {
+    //   await settings.setMigrated();
+    // }
+
+    /// ***************** MIGRATION END *******************
+    settings.appInitialStateIsLoading = false;
+    WidgetsBinding.instance?.addPostFrameCallback((final _) {
+      settings.notify();
+    });
+    return true;
   }
 }

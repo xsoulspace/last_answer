d73b4de1ecf02a39e756627d238ba4cd66ec1088
Anton Malofeev <anton@xsoulspace.dev>
2021-12-24 01:06:22 +0600

    refactor: idea answer screen

--- lib/pack_idea/screens/idea_answer_screen_state.dart
+++ lib/pack_idea/screens/idea_answer_screen_state.dart
@@ -2,30 +2,76 @@ part of pack_idea;
 
 IdeaAnswerScreenState useIdeaAnswerScreenState({
   required final BuildContext context,
+  required final TextEditingController textController,
+  required final StreamController<bool> updatesStream,
+  required final ValueNotifier<IdeaProjectAnswer> answer,
+  required final IdeaProject idea,
+  required final ValueChanged<IdeaProject> onScreenBack,
 }) =>
     use(
       LifeHook(
         debugLabel: 'useIdeaAnswerScreenState',
         state: IdeaAnswerScreenState(
           context: context,
+          textController: textController,
+          updatesStream: updatesStream,
+          answer: answer,
+          idea: idea,
+          onScreenBack: onScreenBack,
         ),
       ),
     );
 
 class IdeaAnswerScreenState implements LifeState {
-  IdeaAnswerScreenState({required final this.context});
+  IdeaAnswerScreenState({
+    required final this.context,
+    required this.textController,
+    required this.updatesStream,
+    required this.answer,
+    required this.idea,
+    required this.onScreenBack,
+  });
   final BuildContext context;
-
+  final TextEditingController textController;
+  final StreamController<bool> updatesStream;
+  final ValueNotifier<IdeaProjectAnswer> answer;
+  final IdeaProject idea;
+  final ValueChanged<IdeaProject> onScreenBack;
   @override
   late ValueChanged<VoidCallback> setState;
 
   @override
-  void dispose() {
-    // TODO: implement dispose
+  void initState() {
+    textController.addListener(onTextChanged);
+
+    updatesStream.stream
+        .sampleTime(
+      const Duration(milliseconds: 700),
+    )
+        .forEach(
+      (final _) async {
+        idea.folder?.sortProjectsByDate(project: idea);
+        // ideaProjectsProvider.notify();
+        await answer.value.save();
+        await idea.save();
+      },
+    );
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
+  void dispose() {
+    textController.removeListener(onTextChanged);
+  }
+
+  void onTextChanged() {
+    if (answer.value.text == textController.text) return;
+    answer.value.text = textController.text;
+    idea.updated = DateTime.now();
+    updatesStream.add(true);
+  }
+
+  void onBack() {
+    closeKeyboard(context: context);
+    onScreenBack(idea);
   }
 }

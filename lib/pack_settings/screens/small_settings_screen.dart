c56d2805b3cc319e39c7f4820ec9c5a2f36af7fb
Anton Malofeev <anton@xsoulspace.dev>
2022-04-16 13:22:33 +0200

    feat: hook state for small settings screen

--- lib/pack_settings/screens/small_settings_screen.dart
+++ lib/pack_settings/screens/small_settings_screen.dart
@@ -12,73 +12,20 @@ class SmallSettingsScreen extends HookWidget {
   @override
   Widget build(final BuildContext context) {
     final routeState = RouteStateScope.of(context);
-    final state = useSmallSettingsScreenStateState();
-
-    final subSettingsPage = useState<Widget?>(null);
-    final pageController = usePageController();
-    final chosenPage = useState(0);
-    Future<void> toPage({final int page = 1}) async {
-      if (!pageController.hasClients) return;
-      chosenPage.value = page;
-      await pageController.animateToPage(
-        page,
-        duration: const Duration(milliseconds: 200),
-        curve: Curves.linearToEaseOut,
-      );
-    }
-
-    Future<void> toNavigation() async => toPage(page: 0);
-
-    Future<void> switchToPage() async {
-      switch (routeState.route.pathTemplate) {
-        case AppRoutesName.profile:
-          subSettingsPage.value = MyAccountScreen(onBack: onBack);
-          await toPage();
-          break;
-        case AppRoutesName.generalSettings:
-          subSettingsPage.value = GeneralSettingsScreen(onBack: onBack);
-          await toPage();
-          break;
-        case AppRoutesName.subscription:
-          subSettingsPage.value = SubscriptionScreen(onBack: onBack);
-          await toPage();
-          break;
-        default:
-          await toNavigation();
-      }
-    }
-
-    pageController.addListener(() {
-      if (!pageController.hasClients) return;
-      final controllerPage = pageController.page?.ceil();
-      if (chosenPage.value == controllerPage) return;
-      onSelectRoute(AppRoutesName.settings);
-    });
-
-    useEffect(
-      () {
-        switchToPage();
-
-        return null;
-      },
-      [routeState.route],
-    );
     final screenLayout = ScreenLayout.of(context);
-    useEffect(
-      () {
-        WidgetsBinding.instance?.addPostFrameCallback((final _) {
-          switchToPage();
-        });
-
-        return null;
-      },
-      [screenLayout.small],
+    final state = useSmallSettingsScreenStateState(
+      onBack: onBack,
+      onSelectRoute: onSelectRoute,
+      routeState: routeState,
+      screenLayout: screenLayout,
     );
-    final subPage = subSettingsPage.value;
+    useSmallSettingsScreenEffects(state: state);
+
+    final subPage = state.subSettingsPage.value;
 
     return PageView(
       physics: const SpeedyPageViewScrollPhysics(),
-      controller: pageController,
+      controller: state.pageController,
       children: [
         SettingsNavigationScreen(
           onBack: onBack,
--- lib/pack_settings/screens/small_settings_screen_state.dart
+++ lib/pack_settings/screens/small_settings_screen_state.dart
@@ -1,25 +1,113 @@
 part of pack_settings;
 
-SmallSettingsScreenState useSmallSettingsScreenStateState() => use(
+SmallSettingsScreenState useSmallSettingsScreenStateState({
+  required final RouteState routeState,
+  required final ValueChanged<AppRouteName> onSelectRoute,
+  required final VoidCallback onBack,
+  required final ScreenLayout screenLayout,
+}) =>
+    use(
       LifeHook(
         debugLabel: 'SmallSettingsScreenState',
-        state: SmallSettingsScreenState(),
+        state: SmallSettingsScreenState(
+          routeState: routeState,
+          screenLayout: screenLayout,
+          onSelectRoute: onSelectRoute,
+          onBack: onBack,
+        ),
       ),
     );
 
 class SmallSettingsScreenState implements LifeState {
-  SmallSettingsScreenState();
-
+  SmallSettingsScreenState({
+    required this.routeState,
+    required this.onSelectRoute,
+    required this.onBack,
+    required this.screenLayout,
+  });
+  final RouteState routeState;
   @override
   ValueChanged<VoidCallback>? setState;
+  final ValueChanged<AppRouteName> onSelectRoute;
+  final VoidCallback onBack;
+  final pageController = PageController();
+  final chosenPage = ValueNotifier<int>(0);
+  final subSettingsPage = ValueNotifier<Widget?>(null);
+  final ScreenLayout screenLayout;
 
   @override
   void initState() {
-    // TODO: implement initState
+    pageController.addListener(onPageCahnged);
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    pageController
+      ..removeListener(onPageCahnged)
+      ..dispose();
+    chosenPage.dispose();
+    subSettingsPage.dispose();
   }
+
+  void onPageCahnged() {
+    if (!pageController.hasClients) return;
+    final controllerPage = pageController.page?.ceil();
+    if (chosenPage.value == controllerPage) return;
+    onSelectRoute(AppRoutesName.settings);
+  }
+
+  Future<void> toPage({final int page = 1}) async {
+    if (!pageController.hasClients) return;
+    chosenPage.value = page;
+    await pageController.animateToPage(
+      page,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.linearToEaseOut,
+    );
+  }
+
+  Future<void> toNavigation() async => toPage(page: 0);
+
+  Future<void> switchToPage() async {
+    switch (routeState.route.pathTemplate) {
+      case AppRoutesName.profile:
+        subSettingsPage.value = MyAccountScreen(onBack: onBack);
+        await toPage();
+        break;
+      case AppRoutesName.generalSettings:
+        subSettingsPage.value = GeneralSettingsScreen(onBack: onBack);
+        await toPage();
+        break;
+      case AppRoutesName.subscription:
+        subSettingsPage.value = SubscriptionScreen(onBack: onBack);
+        await toPage();
+        break;
+      default:
+        await toNavigation();
+    }
+  }
+}
+
+void useSmallSettingsScreenEffects({
+  required final SmallSettingsScreenState state,
+}) {
+  useEffect(
+    () {
+      state.switchToPage();
+
+      return null;
+    },
+    [state.routeState.route],
+  );
+
+  useEffect(
+    () {
+      WidgetsBinding.instance?.addPostFrameCallback((final _) {
+        state.switchToPage();
+      });
+
+      return null;
+    },
+    [state.screenLayout.small],
+  );
 }

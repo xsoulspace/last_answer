32f43819326067f79cbd353046b77e66aa45db66
Anton Malofeev <anton@xsoulspace.dev>
2022-06-08 17:30:47 +0200

    refactor: la_utils_core -> la_core

--- lib/library/widgets/core/state_loader.dart
+++ lib/library/widgets/core/state_loader.dart
@@ -1,9 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lastanswer/library/hooks/hooks.dart';
 import 'package:lastanswer/library/theme/theme.dart';

73ff066d193facbd60651c7bca59f45bead64b73
Arenukvern <anton@xsoulspace.dev>
2023-10-13 00:42:36 +0200

    chore: flutter sdk

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,11 +5,11 @@
 import FlutterMacOS
 import Foundation
 
-import package_info_plus_macos
-import path_provider_macos
+import package_info_plus
+import path_provider_foundation
 import purchases_flutter
-import share_plus_macos
-import shared_preferences_macos
+import share_plus
+import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {

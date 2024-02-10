bf893d03a58b40dca64475fc9941c9e407aa006c
Arenukvern <anton@xsoulspace.dev>
2024-02-10 22:11:33 +0200

    chore: fvm updated to 3.0.0

--- packages/core/lib/src/services/purchases_ads/purchases_ads_web.dart
+++ packages/core/lib/src/services/purchases_ads/purchases_ads_web.dart
@@ -14,15 +14,48 @@ final class PurchasesAdsService extends PurchasesAdsBase {
   }
 
   @override
-  Future<void> onLoad() {
-    throw UnimplementedError();
-  }
+  Future<void> onLoad() {}
 }
 
 final class AdInstanceYaWebImpl extends AdInstance {
   @override
   void dispose() {}
 
+  /// https://yandex.ru/support/partner/web/units/types/rewarded.html
   @override
-  Future<RewardModel> show() async => RewardModel(amount: 0, isRewarded: false);
+  Future<RewardModel> show() async {
+// window.yaContextCb.push(() => {
+//       if (Ya.Context.AdvManager.getPlatform() === 'desktop') {
+//         // вызов блока Rewarded для десктопной версии
+//         Ya.Context.AdvManager.render({
+//           blockId: 'R-A-588461-68',
+//           type: 'rewarded',
+//           platform: 'desktop',
+//           onRewarded: (isRewarded) => {
+//               if (isRewarded) {
+//                   // условие получения награды выполнено
+//               } else {
+//                   // условие получения награды не выполнено
+//               }
+//           }
+//         });
+//       } else {
+//         // вызов блока Rewarded для мобильной версии
+//         Ya.Context.AdvManager.render({
+//           blockId: 'R-A-588461-69',
+//           type: 'rewarded',
+//           platform: 'touch',
+//           onRewarded: (isRewarded) => {
+//               if (isRewarded) {
+//                   // условие получения награды выполнено
+//               } else {
+//                   // условие получения награды не выполнено
+//               }
+//           }
+//         });
+//       }
+//   });
+
+    return RewardModel(amount: 0, isRewarded: false);
+  }
 }
---
+++ packages/core/lib/src/services/purchases_ads/yandex/yandex.dart
@@ -0,0 +1,8 @@
+@JS()
+library;
+
+import 'dart:js_interop';
+
+extension type Ya._(JSObject _)
+    implements JSObject {
+  external factory XMLHttpRequest();

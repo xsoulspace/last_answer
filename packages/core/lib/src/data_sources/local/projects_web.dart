58444631d67c67e31b3f2dd5a258c90fac58775a
Arenukvern <anton@xsoulspace.dev>
2024-03-02 21:46:00 +0200

    feat: search projects on web

--- packages/core/lib/src/data_sources/local/projects_web.dart
+++ packages/core/lib/src/data_sources/local/projects_web.dart
@@ -3,28 +3,48 @@ import 'package:shared_models/shared_models.dart';
 
 import '../../../core.dart';
 
+class SearchableContainer<T> {
+  const SearchableContainer({required this.jsonContent, required this.value});
+  final T value;
+  final String jsonContent;
+  bool contains(final String search) => jsonContent.contains(search);
+}
+
+extension on ProjectModel {
+  SearchableContainer<ProjectModel> toSearchableContainer() =>
+      SearchableContainer(
+        value: this,
+        jsonContent: toString(),
+      );
+}
+
 final class ProjectsLocalDataSourceLocalDbImpl
     implements ProjectsLocalDataSource {
   ProjectsLocalDataSourceLocalDbImpl({
     required this.localDb,
   });
   final LocalDbDataSource localDb;
-  final List<ProjectModel> _cache = [];
+  final List<SearchableContainer<ProjectModel>> _cache = [];
   bool _isReversed = false;
   @override
   Future<PaginatedPageResponseModel<ProjectModel>> getPaginated({
     required final PaginatedPageRequestModel<RequestProjectsDto> dto,
   }) async {
     final data = dto.data;
-
     // ignore: avoid_positional_boolean_parameters
     void reverse({final bool force = false}) {
       if (data == null) return;
       if (_isReversed != data.isReversed || force) {
         if (data.isReversed) {
-          _cache.sort((final a, final b) => b.updatedAt.compareTo(a.updatedAt));
+          _cache.sort(
+            (final a, final b) =>
+                b.value.updatedAt.compareTo(a.value.updatedAt),
+          );
         } else {
-          _cache.sort((final a, final b) => a.updatedAt.compareTo(b.updatedAt));
+          _cache.sort(
+            (final a, final b) =>
+                a.value.updatedAt.compareTo(b.value.updatedAt),
+          );
         }
         _isReversed = data.isReversed;
       }
@@ -35,21 +55,29 @@ final class ProjectsLocalDataSourceLocalDbImpl
         key: SharedPreferencesKeys.webProjects.name,
         convertFromJson: ProjectModel.fromJson,
       );
-      _cache.addAll(localItems);
+      _putAll(projects: localItems);
 
       /// first reverse
       reverse(force: true);
     }
 
     reverse();
+    final items = [..._cache];
+    if (data != null) {
+      if (data.search.isNotEmpty) {
+        final search = data.search;
+        items.retainWhere((final e) => e.jsonContent.contains(search));
+      }
+    }
 
-    final int itemsCount = _cache.length;
+    final int itemsCount = items.length;
     final pagesCount = (itemsCount / dto.limit).ceil();
     final start = dto.page * dto.limit;
-    final items = _cache.skip(start).take(dto.limit);
+    final effectiveItems =
+        items.skip(start).take(dto.limit).map((final e) => e.value);
 
     return PaginatedPageResponseModel(
-      values: items.toList(),
+      values: effectiveItems.toList(),
       currentPage: dto.page,
       pagesCount: pagesCount,
     );
@@ -57,62 +85,75 @@ final class ProjectsLocalDataSourceLocalDbImpl
 
   @override
   Future<void> put({required final ProjectModel project}) async {
-    final index = _cache.indexWhere((final e) => e.id == project.id);
+    final container = project.toSearchableContainer();
+    final index = _cache.indexWhere((final e) => e.value.id == project.id);
     if (index >= 0) {
-      _cache[index] = project;
+      _cache[index] = container;
     } else {
-      _cache.insert(0, project);
+      _cache.insert(0, container);
     }
     _saveCache();
   }
 
   @override
   Future<void> remove({required final ProjectModelId id}) async {
-    _cache.removeWhere((final e) => e.id == id);
+    _cache.removeWhere((final e) => e.value.id == id);
     _saveCache();
+  }
+
+  void _putAll({required final Iterable<ProjectModel> projects}) {
+    final itemsContainers = projects.map(
+      (final e) => e.toSearchableContainer(),
+    );
+    _cache
+      ..clear()
+      ..addAll(itemsContainers);
   }
 
   @override
   Future<void> putAll({required final List<ProjectModel> projects}) async {
-    _cache
-      ..clear()
-      ..addAll(projects);
+    _putAll(projects: projects);
     _saveCache();
   }
 
   void _saveCache() => localDb.setItemsList(
         key: SharedPreferencesKeys.webProjects.name,
-        convertToJson: (final v) => v.toJson(),
+        convertToJson: (final v) => v.value.toJson(),
         value: _cache,
       );
 
   @override
   Future<List<ProjectModel>> getAll({final RequestProjectsDto? dto}) async {
-    final data = [..._cache];
+    final items = [..._cache];
     if (dto != null) {
       if (dto.isReversed) {
-        data.sort((final a, final b) => b.updatedAt.compareTo(a.updatedAt));
+        items.sort(
+          (final a, final b) => b.value.updatedAt.compareTo(a.value.updatedAt),
+        );
       } else {
-        data.sort((final a, final b) => a.updatedAt.compareTo(b.updatedAt));
+        items.sort(
+          (final a, final b) => a.value.updatedAt.compareTo(b.value.updatedAt),
+        );
       }
       if (!dto.tagId.isEmpty) {
-        data.removeWhere((final e) => e.tagsIds.contains(dto.tagId));
+        items.removeWhere((final e) => e.value.tagsIds.contains(dto.tagId));
       }
     }
 
-    return data;
+    return items.map((final e) => e.value).toList();
   }
 
   @override
   Future<ProjectModel?> getById({required final ProjectModelId id}) async =>
-      _cache.firstWhereOrNull((final e) => e.id == id);
+      _cache.firstWhereOrNull((final e) => e.value.id == id)?.value;
 
   @override
   Future<List<ProjectModel>> getByIds({
     required final Iterable<ProjectModelId> ids,
   }) async {
-    final map = _cache.toMap(toKey: (final v) => v.id, toValue: (final v) => v);
+    final map =
+        _cache.toMap(toKey: (final e) => e.value.id, toValue: (final e) => e);
 
-    return ids.map((final e) => map[e]).nonNulls.toList();
+    return ids.map((final e) => map[e]?.value).nonNulls.toList();
   }
 }

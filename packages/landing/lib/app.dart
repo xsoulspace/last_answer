64dd7e807ea8b6554dc9facfdc715b35b52e21ae
Arenukvern <anton@xsoulspace.dev>
2024-03-13 01:56:14 +0200

    feat: first page is ready!
    todo: links
    todo: alt stores
    todo:

--- packages/landing/lib/app.dart
+++ packages/landing/lib/app.dart
@@ -58,7 +58,7 @@ class SlideOne extends StatelessComponent {
             edgeInsets: 'pl-6 pr-4 py-3',
             textStyle: 'text-md font-semibold',
           ),
-          title: 'Use App for Free',
+          title: 'Start for free',
         ),
         Spacer.y('8'),
       ],
@@ -71,20 +71,117 @@ class SlideOne extends StatelessComponent {
         BentoGrid(
           cards: [
             ...[
-              (title: 'Note writing'),
-              (title: 'Post limits'),
-              (title: 'Idea brainstorming'),
-              (title: 'Sharing'),
+              (
+                title: '- Wait, another note app? 👀',
+                description:
+                    'Yes 😉. You know, sometimes, it\'s not that hard to get inspiration, '
+                    'but tooo much waiting for the right app will be open. '
+                    'And in that moment - thoughts forgotten and time is lost. '
+                    'So, with Last Answer we\'ve made it. ',
+              ),
+              (
+                title: 'Start writing before data is loaded',
+                description:
+                    'Last Answer mission is to reduce any delays between '
+                    'thought and writing - if you see the buttons '
+                    'to create a Note or an Idea - you can instantly ⚡️ use '
+                    'them without waiting.',
+              ),
+              (
+                title: 'Offline-first 🏡, your ideas has priority',
+                description:
+                    'To give you fast access to your Notes and Ideas the app loads them as fast as possible, and only then, it loads theme, settings and other preferences. All your data stored in your device.',
+              ),
+              (
+                title: 'Make Note',
+                description: 'To convert quickly thoughts to words.',
+              ),
+              (
+                title: 'Write Post ',
+                description:
+                    'You can write draft of post in Last Answer by using character limits specific to social networks. For example Discord has limit 2000:)',
+              ),
+              (
+                title: 'Brainstorm Idea',
+                description:
+                    'You can use various technics, such as 5 why, six sigmas or similar. Just try it',
+              ),
+              (
+                title: 'Share it! ',
+                description:
+                    'Every note is simple plain text which can be easily copied to any app. Ideas a little bit more complex, but they are also support sharing as simple text.',
+              ),
+              (
+                title: "Read Flow",
+                description:
+                    "We write everyday in messangers, and they have certain flow - to read a chat your eye goes from bottom to up, the same way as you would write on typewriter machine - so as the LastAnswer. Every Note or Idea, designed to feel like a focused chat.",
+              ),
+              (
+                title: 'Updates goes down -& up (if needed)',
+                description:
+                    'Last Answer has auto sorting function - you will see most recent notes are which you edited. Just like in messangers:)'
+              ),
+              (
+                title: 'Configurable',
+                description:
+                    'You can change direction of Notes and Ideas in settings. As the Theme and Language.',
+              ),
+              (
+                title: 'Backup & Restore',
+                description:
+                    'You can always save all data to one single file and restore from it.',
+              )
             ].map((e) => Card([
+                  Spacer.y('2'),
                   h3([text(e.title)],
-                      classes: 'font-semibold text-md md:text-2xl'),
-                ]))
+                      classes:
+                          'font-semibold text-3xl tracking-wide md:text-4xl'),
+                  Spacer.y('8'),
+                  p([text(e.description)], classes: 'text-xl'),
+                  Spacer.y('2'),
+                ], classes: ''))
           ],
         ),
       ], classes: 'relative mx-auto max-w-4xl px-4 py-16 md:p-24 text-center'),
     ], classes: 'bg-zinc-300/10 rounded-[28px] shadow');
 
-    yield Spacer.y('16 md:mt-32');
+    yield section([
+      Spacer.y('16 md:mt-20'),
+      div(
+        [
+          h1(
+            // TODO(arenukvern): think about phrase, it's too much:)
+            [text('Never lose thoughts in the breeze of time')],
+            classes: 'text-4xl md:text-5xl tracking-tighter'
+                'font-bold leading-2 md:leading-tight text-stone-900/80',
+          )
+        ],
+        classes: 'max-w-sm md:max-w-md mx-auto',
+      ),
+      Spacer.y('16 md:mt-20'),
+      div(
+        [
+          OpenAppButton(
+            decoration: StyledButtonDecoration.filled.copyWith(
+              edgeInsets: 'pl-6 pr-4 py-3',
+              textStyle: 'text-md font-semibold',
+            ),
+            title: 'Open app',
+          ),
+          Spacer.x('12 md:mr-24'),
+          StyledButton(
+            decoration: StyledButtonDecoration.text,
+            titleText: 'Download',
+            onClick: () => throw UnimplementedError(),
+          ),
+        ],
+        classes: 'flex flex-row items-center justify-center',
+      ),
+      // TODO(arenukvern): add community links,
+      Spacer.y('20 md:mt-24'),
+    ],
+        classes: 'relative mx-auto max-w-5xl text-center '
+            'px-4 pb-4 pt-16 md:py-28 md:pt-28 md:pb-18');
   }
 }
 
@@ -123,7 +220,8 @@ class HeaderAppBar extends StatelessComponent {
             Spacer.x('4')
           ], classes: 'relative flex items-center'),
         ],
-            classes: 'bg-white/70 backdrop-blur-xl max-w-8xl mx-auto py-2 '
+            classes:
+                'bg-white/70 backdrop-blur-md transition max-w-8xl mx-auto py-2 '
                 'border-b border-stone-900/5'),
       ],
       classes: 'sticky top-0 z-50',
@@ -184,6 +282,7 @@ class FotterBottomBar extends StatelessComponent {
           ], classes: 'grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-6'),
           Spacer.y('6'),
           hr(),
+          // TODO(arenukvern): add social icons with links,
           p(
             [text('© 2020-2024 Anton Malofeev, Irina Veter.')],
             classes: 'mt-8 text-xs leading-5 text-stone-500',
--- packages/landing/lib/ui_kit/containers.dart
+++ packages/landing/lib/ui_kit/containers.dart
@@ -1,15 +1,16 @@
 import 'package:jaspr/jaspr.dart';
 
 class Card extends StatelessComponent {
-  const Card(this.children, {this.size = '', super.key});
+  const Card(this.children, {this.classes = '', super.key});
   final List<Component> children;
-  final String size;
+  final String classes;
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield div(children,
-        classes: 'rounded-full border '
-            'border-stone-100/80 bg-white/80 p-4 md:p-8 $size flex items-center');
+        classes: 'rounded-[64px] border '
+            'border-stone-100/80 bg-white/90 p-8 md:p-24 '
+            '$classes flex flex-col items-start text-left');
   }
 }
 
@@ -19,6 +20,7 @@ class BentoGrid extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield div(cards,
-        classes: 'grid grid-flow-row-dense grid-cols-1 sm:grid-cols-2 gap-4');
+        classes:
+            'grid grid-flow-row-dense grid-cols-1 sm:grid-cols-1 gap-16 md:gap-24');
   }
 }
